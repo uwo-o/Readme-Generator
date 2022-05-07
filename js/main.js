504fdd2118e47a7c779cd716129bbeaf883cfc48
uwo-o <hugo.campos@usm.cl>
2022-05-06 23:47:37 -0400

    typed js implemented

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var typed = new Typed('.typed-web', {
+    strings: ["b"],
+    typeSpeed: 100,
+    startDelay: 1000,
+    showCursor: false,
+});
+
+var typed2 = new Typed('.typed-readme', {
+    strings: ["eadme"],
+    typeSpeed: 100,
+    startDelay: 2000,
+    showCursor: false,
+});
+
+var typed3 = new Typed('.typed-generator', {
+    strings: ["erator"],
+    typeSpeed: 100,
+    startDelay: 3000,
+    showCursor: false,
+});
