706cace590c2a30f49f19f6b63b24ae4e6513ef0
uwo-o <hugo.campos@usm.cl>
2022-05-07 12:41:26 -0400

    delay added

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 var typed = new Typed('.typed-web', {
     strings: ["b"],
     typeSpeed: 100,
-    startDelay: 0,
+    startDelay: 1000,
     showCursor: false,
 });
 
 var typed2 = new Typed('.typed-readme', {
     strings: ["eadme"],
     typeSpeed: 100,
-    startDelay: 500,
+    startDelay: 1500,
     showCursor: false,
 });
 
 var typed3 = new Typed('.typed-generator', {
     strings: ["erator"],
     typeSpeed: 100,
-    startDelay: 1000,
+    startDelay: 2000,
     showCursor: false,
 });
