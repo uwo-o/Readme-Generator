6bcc37a334615b495a6d8a1f085fdb3fa7467614
uwo-o <hugo.campos@usm.cl>
2022-05-07 03:04:57 -0400

    bug solved

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-var typed = new Typed('.typed-web', {
-    strings: ["b"],
-    typeSpeed: 100,
-    startDelay: 1000,
-    showCursor: false,
-});
+var title_hover = false;
 
-var typed2 = new Typed('.typed-readme', {
-    strings: ["eadme"],
-    typeSpeed: 100,
-    startDelay: 2000,
-    showCursor: false,
-});
+if(!title_hover) {
+    $(".title").mouseenter(function () {
+        title_hover = true;
+        var typed = new Typed('.typed-web', {
+            strings: ["b"],
+            typeSpeed: 100,
+            startDelay: 0,
+            showCursor: false,
+        });
+    
+        var typed2 = new Typed('.typed-readme', {
+            strings: ["eadme"],
+            typeSpeed: 100,
+            startDelay: 500,
+            showCursor: false,
+        });
+    
+        var typed3 = new Typed('.typed-generator', {
+            strings: ["erator"],
+            typeSpeed: 100,
+            startDelay: 1000,
+            showCursor: false,
+        });
 
-var typed3 = new Typed('.typed-generator', {
-    strings: ["erator"],
-    typeSpeed: 100,
-    startDelay: 3000,
-    showCursor: false,
-});
+    });
+}
