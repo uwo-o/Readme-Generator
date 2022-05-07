ad7f1eba8225dc42fd0531769438d10a788b6398
uwo-o <hugo.campos@usm.cl>
2022-05-07 03:46:48 -0400

    only title animation in the start

--- js/main.js
+++ js/main.js
@@ -1,28 +1,20 @@
-var title_hover = false;
+var typed = new Typed('.typed-web', {
+    strings: ["b"],
+    typeSpeed: 100,
+    startDelay: 0,
+    showCursor: false,
+});
 
-if(!title_hover) {
-    $(".title").mouseenter(function () {
-        title_hover = true;
-        var typed = new Typed('.typed-web', {
-            strings: ["b"],
-            typeSpeed: 100,
-            startDelay: 0,
-            showCursor: false,
-        });
-    
-        var typed2 = new Typed('.typed-readme', {
-            strings: ["eadme"],
-            typeSpeed: 100,
-            startDelay: 500,
-            showCursor: false,
-        });
-    
-        var typed3 = new Typed('.typed-generator', {
-            strings: ["erator"],
-            typeSpeed: 100,
-            startDelay: 1000,
-            showCursor: false,
-        });
+var typed2 = new Typed('.typed-readme', {
+    strings: ["eadme"],
+    typeSpeed: 100,
+    startDelay: 500,
+    showCursor: false,
+});
 
-    });
-}
+var typed3 = new Typed('.typed-generator', {
+    strings: ["erator"],
+    typeSpeed: 100,
+    startDelay: 1000,
+    showCursor: false,
+});
