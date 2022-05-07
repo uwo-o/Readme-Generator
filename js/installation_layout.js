3af94e9f23007522fd0634cb022d88212cb9eb1c
uwo-o <hugo.campos@usm.cl>
2022-05-07 12:41:14 -0400

    add commands in installation improved

--- js/installation_layout.js
+++ js/installation_layout.js
@@ -1,4 +1,5 @@
 var counter_installation_step=1;
+var counter_installation_command=[];
 
 if(counter_installation_step==1){
     $("#btn-delete-installation-step").hide();
@@ -21,9 +22,15 @@ $(".btn-add-installation-step").click(function(e){
                     <label for="step`+counter_installation_step+`-description">Description</label>\
                     <input name="step`+counter_installation_step+`-description" class="form-control" id="step`+counter_installation_step+`-description">\
                 </div>\
+                <div class="container" id="container-installarion-commands-`+counter_installation_step+`">\
+                </div>
                 <div class="row">\
-                    <label for="step`+counter_installation_step+`-command">Command</label>\
-                    <input type="text" class="form-control" name="step`+counter_installation_step+`-command" id="step`+counter_installation_step+`-command">\
+                <div class="col-md-2">\
+                        <button class="btn btn-primary btn-add-installation-command" id="btn-add-installation-command-`+counter_installation_step+`">Add command</button>\
+                    </div>\
+                    <div class="col-md-2">\
+                        <button class="btn btn-delete btn-delete-installation-command" id="btn-delete-installation-command-`+counter_installation_step+`">Delete command</button>\
+                    </div>\
                 </div>\
             </div>\
         </div>\
@@ -32,6 +39,11 @@ $(".btn-add-installation-step").click(function(e){
     $("#installation-container").append(html);
     $("#btn-delete-installation-step").show();
     counter_installation_step++;
+    id_step=counter_installation_step-1;
+    counter_installation_command[id_step]=counter_installation_command[id_step]?counter_installation_command[id_step]:1;
+    if(counter_installation_command[id_step]<=1){
+        $("#btn-delete-installation-command-"+(counter_installation_step-1)).hide();
+    }
 });
 
 $(document).on('click', '#btn-delete-installation-step', function(e){
@@ -41,5 +53,41 @@ $(document).on('click', '#btn-delete-installation-step', function(e){
     if(counter_installation_step<=1){
         $("#btn-delete-installation-step").hide();
         counter_installation_step=1;
+    }
+    counter_installation_command.pop();
+});
+
+$(document).on('click', '.btn-add-installation-command', function(e){
+    e.preventDefault();
+    let id_step=$(this).attr("id").split("-")[4];
+    let html=`
+    <div class="row" id="installation-`+id_step+`-command-`+counter_installation_command[id_step]+`">\
+        <div class="col-md-12">\
+            <div class="form-group">\
+                <div class="row">\
+                    <label>Command `+counter_installation_command[id_step]+`</label>\
+                    <input type="text" class="form-control" name="step`+id_step+`-command`+counter_installation_command[id_step]+`" id="step`+id_step+`-command`+counter_installation_command[id_step]+`">\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+`;
+    $("#container-installarion-commands-"+id_step).append(html);
+    counter_installation_command[id_step]++;
+    if(counter_installation_command[id_step]>1){
+        $("#btn-delete-installation-command-"+id_step).show();
+    }
+});
+
+
+$(document).on('click', '.btn-delete-installation-command', function(e){
+    e.preventDefault();
+    let id_step=$(this).attr("id").split("-")[4];
+    let id_command=$(this).attr("id").split("-")[5];
+    counter_installation_command[id_step]--;
+    $("#installation-"+id_step+"-command-"+counter_installation_command[id_step]).remove();
+    if(counter_installation_command[id_step]<=1){
+        counter_installation_command[id_step]=1;
+        $("#btn-delete-installation-command-"+id_step).hide();
     }
 });
